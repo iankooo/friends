93acbf32249aa12ab9c789e17548e828c53c640d
ENDAVA\ischipala <ianko.schipala@endava.com>
2022-06-29 17:33:13 +0300

    new edit

--- app/src/androidTest/java/sir/ianko/friends/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/sir/ianko/friends/ExampleInstrumentedTest.kt
@@ -1,10 +1,12 @@
 package sir.ianko.friends
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
 import org.junit.Test
 import org.junit.runner.RunWith
+
+import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,7 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().context
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.friends", appContext.packageName)
     }
 }
